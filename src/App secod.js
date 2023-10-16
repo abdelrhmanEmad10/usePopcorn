00247858a3bd86ea68b6def9289227976f309cf5
Abdelrhman_emad <121755506+abdelrhmanEmad10@users.noreply.github.com>
2023-10-16 20:45:40 +0300

    last patch now

--- src/App secod.js
+++ src/App secod.js
@@ -1,52 +1,5 @@
 import { useState } from "react";
 
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
-
-const tempWatchedData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt0088763",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
-
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
@@ -71,32 +24,38 @@ function Main({ children }) {
 
 function Box({ children }) {
   const [isOpen1, setIsOpen1] = useState(true);
-  return <div className="box">
-    <button className="btn-toggle" onClick={() => setIsOpen1((open) => !open)}>
-      {isOpen1 ? "–" : "+"}
-    </button>
-    {isOpen1 && children}
-  </div>;
+  return (
+    <div className="box">
+      <button
+        className="btn-toggle"
+        onClick={() => setIsOpen1((open) => !open)}
+      >
+        {isOpen1 ? "–" : "+"}
+      </button>
+      {isOpen1 && children}
+    </div>
+  );
 }
 
 function MoviesList({ movies }) {
   return (
     <ul className="list">
-          {movies?.map((movie) => (
-            <Movie movie={movie} key={movie.imdbID} />
-          ))}
-        </ul>
+      {movies?.map((movie) => (
+        <Movie movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
   );
 }
 
-function WatchedList({watched}) { 
+function WatchedList({ watched }) {
   return (
-          <ul className="list">
-            {watched.map((movie) => (
-              <WatchedMovie movie={movie} />
-            ))}
-          </ul>
-      )}
+    <ul className="list">
+      {watched.map((movie) => (
+        <WatchedMovie movie={movie} />
+      ))}
+    </ul>
+  );
+}
 function WatchedListSummary({ watched }) {
   const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
   const avgUserRating = average(watched.map((movie) => movie.userRating));
@@ -181,7 +140,7 @@ export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
 
-  fetch("http://www.omdbapi.com/?apikey=&")
+  fetch("http://www.omdbapi.com/?apikey=&");
   return (
     <>
       <Navbar>{movies.length}</Navbar>
@@ -192,7 +151,7 @@ export default function App() {
 
         <Box>
           <WatchedListSummary watched={watched} />
-          <WatchedList watched={watched}/>
+          <WatchedList watched={watched} />
         </Box>
       </Main>
     </>
